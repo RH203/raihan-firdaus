733f40325e67e6a766f49b983f56743ec201094e
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-07 17:10:32 +0700

    chore: Update Navbar component with scroll-to-section functionality

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import {RiMenu3Line} from "react-icons/ri";
 import {useState} from "react";
+import {scrollToSection} from "../../utils/utils.js";
 
 function Navbar() {
   const [rotated, setRotated] = useState(false);
@@ -7,16 +8,28 @@ function Navbar() {
   const toggle = () => setRotated(!rotated)
 
   return (
-    
     <div className="navbar bg-base-100 font-poppins">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl" href={"profile"}>Raihan Firdaus</a>
       </div>
       <div className="flex-none lg:block md:block sm:hidden">
-        <ul className="menu menu-horizontal px-1">
-          <li><a>Skill</a></li>
-          <li><a>Experience</a></li>
-          <li><a>Contact me</a></li>
+        <ul className="menu menu-horizontal px-1 space-x-4">
+          <li>
+            <div onClick={() => scrollToSection('skill')}>
+              Skill
+            </div>
+          </li>
+          <li>
+            <div onClick={() => scrollToSection('experience')}>
+              Experience
+            </div>
+          </li>
+          <li>
+            <div onClick={() => scrollToSection('contact')}>
+              Contact me
+            </div>
+          </li>
+
         </ul>
       </div>
       <div className="dropdown dropdown-bottom dropdown-end lg:hidden md:hidden sm:block">
@@ -25,10 +38,23 @@ function Navbar() {
             rotated ? 'rotate-180' : ''
           }`}/>
         </div>
-        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
-          <li><a>Skill</a></li>
-          <li><a>Experience</a></li>
-          <li><a>Contact me</a></li>
+        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow ">
+          <li>
+            <div onClick={() => scrollToSection('skill')}>
+              Skill
+            </div>
+          </li>
+          <li>
+            <div onClick={() => scrollToSection('experience')}>
+              Experience
+            </div>
+          </li>
+          <li>
+            <div onClick={() => scrollToSection('contact')}>
+              Contact me
+            </div>
+          </li>
+
         </ul>
       </div>
     </div>
