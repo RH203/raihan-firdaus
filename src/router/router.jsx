6503b530374dd841f053cd17bab5bbfb1702884e
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-07-08 19:19:32 +0700

    chore: Update router.jsx to include BlogPage and ErrorPage components
    This commit updates the router.jsx file to include the BlogPage and ErrorPage components. The BlogPage component is added as a child route to the root path ("/"), and the ErrorPage component is added as a fallback route for any other paths. This change allows the application to display the blog page and a 404 error page when necessary.

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,7 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "../pages/_layout/_app.jsx";
-import {LandingPage} from "../pages/index.js";
+import {BlogPage, LandingPage} from "../pages/index.js";
+import {ErrorPage} from "../components/index.js";
 
 
 export const router = createBrowserRouter([
@@ -9,9 +10,21 @@ export const router = createBrowserRouter([
     element: <App/>,
     children: [
       {
-        path: "",
+        path: "/",
+        element: <BlogPage/>
+      },
+      {
+        path: "about-me",
         element: <LandingPage/>
-      }
+      },
+      {
+        path: "*",
+        element: <ErrorPage/>
+      },
     ]
-  }
+  },
+  {
+    path: "*",
+    element: <ErrorPage/>
+  },
 ]);
